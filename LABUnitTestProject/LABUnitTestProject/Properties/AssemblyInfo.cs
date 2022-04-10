014d8bbd3c8c75ea9b1cc63f03ea6d950858c6ab
natboon <natthida.bo@kkumail.com>
2022-04-11 00:24:31 +0700

    unittest

---
+++ LABUnitTestProject/LABUnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LABUnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LABUnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("56e0d7dc-1640-4fab-8734-ca02508d0738")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LABUnitTestProject/LABUnitTestProject/UnitTest2.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasslibrary;
+using System;
+
+namespace LABUnitTestProject
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void Test_Assignment()
+        {
+            Assignment ac = new Assignment();
+            int input = 20;
+            string result = ac.greadCalculate(input);
+            Assert.AreEqual("F", result);
+
+            //result = ac.greadCalculate(-1);
+            //Assert.AreEqual("F", result);
+        }
+        [TestMethod]
+        public void Test_Assignment_2()
+        {
+            Assignment ac = new Assignment();
+            double result = ac.getDistance(3, 2, 7, 8);
+            Assert.AreEqual(7.21, result);
+            result = ac.getDistance(-3, 2, 7, -8);
+            Assert.AreEqual(14.14, result);
+            result = ac.getDistance(-3, -2, -7, -8);
+            Assert.AreEqual(7.21, result);
+            result = ac.getDistance(3, -2, -7, 8);
+            Assert.AreEqual(14.14, result);
+        }
+        [TestMethod]
+        public void Test_Assignment_3()
+        {
+            Assignment ac = new Assignment();
+            int result = ac.getFactorial(4);
+            Assert.AreEqual(24, result);
+        }
+        [TestMethod]
+        public void Test_AssignmentClass_4()
+        {
+
+            Assignment ac = new Assignment();
+            string Password = "nattcatt";
+            bool password = ac.checkPassword(Password);
+            Assert.AreEqual(true, password);
+        }
+    }
+}
